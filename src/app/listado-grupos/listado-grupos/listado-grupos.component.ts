6538736a500a874c2dc3c4213a3de780ee5b509c
CORP\apastorm <albert.pastor-martinez-external@capgemini.com>
2021-03-12 11:19:00 +0100

    no message

--- src/app/listado-grupos/listado-grupos/listado-grupos.component.ts
+++ src/app/listado-grupos/listado-grupos/listado-grupos.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { PageEvent } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
+import { ListadoGrupos } from '../model/ListadoGrupos';
 import { Pageable } from '../page/Pageable';
 import { ListadoGruposService } from '../services/listado-grupos.service';
 import { ListadoGruposDialogComponent } from './listado-grupos-dialog/listado-grupos-dialog.component';
@@ -17,7 +18,7 @@ export class ListadoGruposComponent implements OnInit {
   pageSize = 20;
   totalElements = 0;
 
-  dataSource = new MatTableDataSource<ListadoGruposComponent>();
+  dataSource = new MatTableDataSource<ListadoGrupos>();
   displayedColumns: string[] = ['id', 'nombre', 'gestores', 'numPersonas', 'numSubGrupos', 'actions'];
 
   constructor(
@@ -44,10 +45,18 @@ export class ListadoGruposComponent implements OnInit {
     }
 
     this.listadoGruposService.getGrupos(pageable).subscribe(data => {
-      this.dataSource.data = data.content;
-      this.pageNumber = data.pageable.pageNumber;
-      this.pageSize = data.pageable.pageSize;
-      this.totalElements = data.totalElements;
+      if (data.content != null) {
+        this.dataSource.data = data.content;
+      }
+      if (data.pageable?.pageNumber != null) {
+        this.pageNumber = data.pageable.pageNumber;
+      }
+      if (data.pageable?.pageSize != null) {
+        this.pageSize = data.pageable.pageSize;
+      }
+      if (data.totalElements != null) {
+        this.totalElements = data.totalElements;
+      }
     });
   }
 
