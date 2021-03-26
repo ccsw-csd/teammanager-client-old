2665845d056ab2262420114ea7338eb21583aa73
CORP\raquesan <raquel.sanchez-celada-external@capgemini.com>
2021-03-26 08:20:19 +0100

    Calendario personal y componente calendario

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CoreModule } from './core/core.module';
 import { LoginModule } from './login/login.module';
 import { PersonalCalendarModule } from './personal-calendar/personal-calendar.module';
+import { MonthCalendarModule } from './month-calendar/month-calendar.module';
+import { MatDatepickerModule} from '@angular/material/datepicker';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatSelectModule } from '@angular/material/select';
+
 
 @NgModule({
   declarations: [
@@ -19,6 +24,11 @@ import { PersonalCalendarModule } from './personal-calendar/personal-calendar.mo
     CoreModule,
     LoginModule,
     PersonalCalendarModule,
+    MonthCalendarModule,
+    MatDatepickerModule,
+    MatGridListModule,
+    FormsModule,
+    MatSelectModule
   ],
   providers: [],
   bootstrap: [AppComponent]
