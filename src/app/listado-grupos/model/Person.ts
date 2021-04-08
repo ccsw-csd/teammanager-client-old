5dee1933130fa9e230e0fb70ae1ba715696238b1
AOlmosCabrelles <a.olmoscabrelles@gmail.com>
2021-04-08 12:43:16 +0200

    Merge branch 'develop' into feature/F-02.2Develop
    # Conflicts:
    #	src/app/core/month-calendar/month-calendar/month-calendar.component.ts
    #	src/app/forecast-list/forecast-detail/forecast-detail.component.ts

---
+++ src/app/listado-grupos/model/Person.ts
@@ -0,0 +1,11 @@
+export class Person {
+  id?: number;
+  saga?: string;
+  username?: string;
+  email?: string;
+  name?: string;
+  lastname?: string;
+  centerId?: number;
+  bussinescode?: string;
+  active?: boolean;
+}
--- src/app/listado-grupos/services/listado-grupos.service.ts
+++ src/app/listado-grupos/services/listado-grupos.service.ts
@@ -4,18 +4,35 @@ import { ListadoGruposPage } from '../model/ListadoGruposPage';
 import { Pageable } from '../../core/to/Pageable';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
-
+import { Group } from '../model/Group';
+import { Person } from '../model/Person';
+import { group } from '@angular/animations';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ListadoGruposService {
 
+  getManagerUsername(usuario: string): Observable<Person> {
+    return this.http.post<Person>(environment.server + '/grouplist/grupoNuevo/', usuario);
+  }
   getGrupos(pageable: Pageable): Observable<ListadoGruposPage> {
     return this.http.post<ListadoGruposPage>(environment.server + '/grouplist/', {pageable});
+  }
+  getGroup(id: number): Observable<Group>{
+    return this.http.post<Group>(environment.server + '/grouplist/editgroup/', id);
+  }
+  getPersons(prefix: string): Observable<Person[]> {
+    return this.http.post<Person[]>(environment.server + '/grouplist/persons/', prefix);
+  }
+  getSubgroups(prefix: string): Observable<Group[]> {
+    return this.http.post<Group[]>(environment.server + '/grouplist/subgroups/', prefix);
+  }
+  saveGroup(newGroup: Group): Observable<Group>{
+    return this.http.put<Group>(environment.server + '/grouplist/', newGroup);
   }
 
   constructor(
     private http: HttpClient
-) { }
+  ) { }
 }
